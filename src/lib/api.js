2b2b5de026e5e520fc0c711db64f11e294e37d00
Muttahar Sheikh <153789569+muttahar123@users.noreply.github.com>
2026-02-28 14:38:45 +0500

    update backend api

--- src/lib/api.js
+++ src/lib/api.js
@@ -1,3 +1,5 @@
 // Centralized API configuration
 // export const BASE_URL = 'http://localhost:5000';
-export const BASE_URL = 'https://cashmish-backend.onrender.com';
+// export const BASE_URL = 'https://cashmish-backend.onrender.com';
+export const BASE_URL = 'http://192.168.1.184:5000';
+
